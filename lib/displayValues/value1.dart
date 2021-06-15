c3c67f6aab9f3fe3facf4bb36130b5dcf94ec66f
RayanIsmael <rayanalan6@gmail.com>
2021-06-15 17:40:43 +0300

    add how to user provider

--- lib/displayValues/value1.dart
+++ lib/displayValues/value1.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_test1/myclass/myclass.dart';
 
 class Value1 extends StatelessWidget {
   const Value1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var mypro = Provider.of<MyClass>(context);
     return Card(
       child: Container(
         height: 200,
         width: 200,
         alignment: Alignment.center,
-        child: Text("Value 1"),
+        child: Text(mypro.value1 == null ? "Value 1":"${mypro.value1}"),
       ),
     );
   }
--- lib/displayValues/value2.dart
+++ lib/displayValues/value2.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_test1/myclass/myclass.dart';
 
 class Value2 extends StatelessWidget {
   const Value2({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var mypro = Provider.of<MyClass>(context);
     return Card(
       child: Container(
         height: 200,
         width: 200,
         alignment: Alignment.center,
-        child: Text("Value 2"),
+        child: Text(mypro.value2 == null ? "Value 2":"${mypro.value2}"),
       ),
     );
   }
